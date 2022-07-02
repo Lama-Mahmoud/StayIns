ec162ab19b590e8073b6851934bf56c4bf7061b3
lama mahmoud <lamamahmoud213@gmail.com>
2022-07-02 14:30:48 +0300

    Editing Files and installing packages

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,30 @@
-const { Router } = require('express');
-const { login, addStayin,stayIn } = require('./controller/user');
-const router = Router();
+require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const port = 3001;
 
-router.post("auth/login",login);
-router.post("auth/addStayin",addStayin);
-router.get("stayIns",stayIn)
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+const userRouter = require('./src/user');
+
+
+const DB_CONNECT = process.env.DB_CONNECT || "";
+mongoose.connect(
+  DB_CONNECT,
+  (err) => 
+ { if(!err)
+    console.log("connected succesfully");
+  else
+    console.log(err);
+}
+);
+
+app.use('/api/user', userRouter);
+
+app.listen(
+    port,
+    () => console.log(`Listening on port ${port}`)
+  );
---
+++ backend/src/user/index.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+const { login, addStayin,stayIn } = require('./controller/user');
+const router = Router();
+
+router.post("auth/login",login);
+router.post("auth/addStayin",addStayin);
+router.get("stayIns",stayIn);
+router.post("favourit",fav);
+
+module.exports = router;
