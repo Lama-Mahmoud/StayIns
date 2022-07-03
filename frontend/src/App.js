9e170a34a7a9efb4fb8bb659fbb8618782eed7ea
lama mahmoud <lamamahmoud213@gmail.com>
2022-07-03 03:41:23 +0300

    Frontendg adding routes to app js, adding dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Add from "./pages/addStay";
+import Guest from "./pages/Guest";
 
 function App() {
   return (
-    <div className="App">
-        
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="Admin" element={<Login/>}/>
+        <Route path="Admin/Add" element={<Add/>}/>
+        <Route path="Guest" element={<Guest/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
